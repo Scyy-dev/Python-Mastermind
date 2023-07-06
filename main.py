f7fa8b378b1ceca1fac89e1a71f9cafb98f3f5d8
Scyphers <scyphers.dev@gmail.com>
2023-07-07 00:16:12 +0800

    clarify purpose of pegs in code
    Rather than rely on knowledge of the 'pegs', this assumes minimal familiarity with the Mastermind game

--- main.py
+++ main.py
@@ -9,11 +9,10 @@ while True:
     secret = str(random.randint(100, 999))
     
     while turns_left > 0:
-        white_peg = 0
-        black_peg = 0
+        number_correct = 0
+        position_correct = 0
         
         guess = input("Guess a number:")
-        
         while len(guess) != 3:
             print("Too small! Must be 100-999")
             guess = input("Guess a number: ")
@@ -27,17 +26,17 @@ while True:
         
         for g, s in zip(guess, secret):
             if g == s:
-                black_peg += 1
+                position_correct += 1
             else:
                 guess_list.append(g)
                 secret_list.append(s)
             
         for g in guess_list:
             if g in secret_list:
-                white_peg += 1
+                number_correct += 1
                 secret_list.remove(g)
                 
-        print("That guess got", black_peg, "black pegs and", max(white_peg, 0), "white pegs \n")
+        print("That guess got", position_correct, "numbers in the right position and", number_correct, "other correct numbers.")
         turns_left -= 1
         
     if turns_left == 0:   
